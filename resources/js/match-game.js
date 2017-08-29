e37a09371debf93eb85c35e54fd0ed709d6bc90f
Laurel Carpenter <laurel_carpenter@me.com>
2017-08-28 22:33:07 -0400

    First draft of Match Game without card flip.

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -4,14 +4,27 @@ var MatchGame = {};
   Sets up a new game after HTML document has loaded.
   Renders a 4x4 board of cards.
 */
-
+$(document).ready(function() {
+ 	MatchGame.renderCards(MatchGame.generateCardValues(), $('#game'));
+ });
 /*
   Generates and returns an array of matching card values.
  */
 
 MatchGame.generateCardValues = function () {
-
-};
+    var cardValues = [], randomCardValues = [], randomNumber, randomIndex, arrayValue;
+    for (var i = 1; i <= 8; i++) {
+        cardValues.push(i);
+        cardValues.push(i);
+    }
+     while (cardValues.length > 0) {
+         randomIndex = cardValues.length;
+         randomNumber = Math.floor(Math.random() * randomIndex);
+         arrayValue = cardValues.splice (randomNumber, 1)[0];
+         randomCardValues.push(arrayValue);
+     }
+     return randomCardValues;
+}
 
 /*
   Converts card values to jQuery card objects and adds them to the supplied game
@@ -19,7 +32,32 @@ MatchGame.generateCardValues = function () {
 */
 
 MatchGame.renderCards = function(cardValues, $game) {
+$game.data('flippedCards', []);
+ $game.data('gameFlippedCards',[]);
+ 	
+ var colArray = [
+'hsl(25, 85%, 65%)',
+'hsl(55, 85%, 65%)',
+'hsl(90, 85%, 65%)',
+'hsl(160, 85%, 65%)',
+'hsl(220, 85%, 65%)',
+'hsl(265, 85%, 65%)',
+'hsl(310, 85%, 65%)',
+'hsl(360, 85%, 65%)'
+];
 
+$game.html('');
+for (var i = 0; i < cardValues.length; i++) {
+	var $card = $('<div class="col-xs-3 card"></div>');
+	$card.data('value', cardValues[i]);
+	$card.data('flipped', false);
+	$card.data('color', colArray[cardValues[i]-1]);
+	$game.append($card);
+	
+	$card.click(function() {
+		MatchGame.flipCard($(this), $game);
+	});
+}
 };
 
 /*
